5dd552a50666ac8e6e8a3766d431e72b6ed3693c
ZacMansour <47873884+ZacMansour@users.noreply.github.com>
2019-03-01 11:59:49 +1100

    End of Week 3

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //right click to rename variables so the rest in the script changes as well
+    public Light myLight;
+    public int health = 100;
+    public int healthBonus = 10;
+    public float speed = 0.05f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*normal color creates basic primary and secondary colors
+         * whereas new color allows you to control RGB setting with
+         * 1 being the highest value instead of it being 255*/
+        myLight.color = Color.blue;
+        
+        //myLight.color = new Color(1, 1, 1);
+        AddToHealth();
+
+        //Adds full health
+        AddToHealth(100);
+
+        //Adds half health
+        AddToHealth(50);
+
+        //Adds tiny health
+        AddToHealth(10);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.H))
+        {
+            AddToHealth(1);
+        }
+
+        if(Input.GetKeyDown(KeyCode.J))
+        {
+            AddToHealth(50);
+            Debug.Log("Big Health");
+        }
+
+        if (Input.GetKeyUp(KeyCode.J))
+        {
+            AddToHealth(150);
+            Debug.Log("Big Health Up");
+        }
+    }
+
+    void Movement()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        transform.Translate(new Vector3(h, 0, v) * speed);
+    }
+
+    //both mean the same thing but += is just shorter
+    //adds to our health
+    void AddToHealth()
+    {
+       //health = health + 10;
+        health += healthBonus;
+    }
+
+    /*words in brackets allow for AddToHealth to be used again in a differing way
+     * if taken out it will not work as Unity can see two of the same functions*/
+    void AddToHealth(int healthModifier)
+    {
+        health += healthModifier;
+    }
+
+}
