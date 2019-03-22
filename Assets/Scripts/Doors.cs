ccb6c4b45ff7f84c71e661a90d63f6aa2663b3c4
ZacMansour <47873884+ZacMansour@users.noreply.github.com>
2019-03-22 11:53:55 +1100

    Week 6

---
+++ Assets/Scripts/Doors.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Doors : MonoBehaviour
+{
+    public Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        anim.SetTrigger("DoorOpen");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        anim.SetTrigger("DoorClose");
+    }
+
+}
