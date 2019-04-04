7b83ff7b4a5deed67df2fbf9cda61787eb1033d1
ZacMansour <47873884+ZacMansour@users.noreply.github.com>
2019-04-05 10:55:26 +1100

    Week 8

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    GameObject player;
+    public float dist;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(player.transform);
+
+        dist = Vector3.Distance(transform.position, player.transform.position);
+
+        //if player distance greater than 15 units stop looking at player
+        if (dist <= 15)
+        {
+            transform.LookAt(player.transform);
+        }
+
+        //if player is closer than three units change colour to blue
+        if (dist <= 3)
+        {
+            GetComponent<Renderer>().material.color = Color.blue;
+        }
+        else
+            GetComponent<Renderer>().material.color = Color.red;
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject spawnObject;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+            Spawn();
+    }
+
+    void Spawn()
+    {
+        int rndX = Random.Range(-15,15);
+        int rndZ = Random.Range(-15, 15);
+        Vector3 spawnPos = new Vector3(rndX, 0, rndZ);
+        Instantiate(spawnObject, spawnPos, transform.rotation);
+    }
+}
