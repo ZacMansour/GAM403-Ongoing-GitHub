8865bd7ae959eb57f2b5a63f6f133c12eb8588f3
ZacMansour <47873884+ZacMansour@users.noreply.github.com>
2019-04-26 10:40:06 +1000

    Week 11

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject pausePanel;
+    public bool isPaused = false;
+
+    public void LoadLevel(string levelName)
+    {
+        SceneManager.LoadScene(levelName);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && pausePanel != null)
+        {
+            //Equals to what pause is not (the opposite)
+            //Toggles the bool and sets canvas appropriately
+            isPaused = !isPaused;
+            pausePanel.SetActive(isPaused);
+        }
+    }
+
+}
