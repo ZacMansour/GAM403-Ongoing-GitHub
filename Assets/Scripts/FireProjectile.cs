27bd6234fbebf72e6310150493b4adc34fee105a
ZacMansour <47873884+ZacMansour@users.noreply.github.com>
2019-05-05 20:42:45 +1000

    Complete

--- Assets/Scripts/FireProjectile.cs
+++ Assets/Scripts/FireProjectile.cs
@@ -6,13 +6,14 @@ public class FireProjectile : MonoBehaviour
 {
     public Weapons weapons;
     public Transform firingPoint;
+    public int speed = 10;
 
     // Update is called once per frame
     void Update()
     {
        if(Input.GetButtonDown("Fire1"))
         {
-            Instantiate(weapons.currentProjectile,firingPoint.position,firingPoint.rotation);
+            Instantiate(weapons.currentProjectile,firingPoint.position, firingPoint.rotation);
         }
     }
 }
